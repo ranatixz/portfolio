da06c148c2ded858970c3a1c2487fab2768cd4f2
peenest <lanchitavincent@gmail.com>
2020-08-28 01:11:20 +0800

    fixing navbar bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import Link from 'react-router-dom'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Navbar = () =>{
+
+    const [navBar, setNavBar] = useState(false)
+
+    useEffect(()=>{
+      Aos.init({ duration:2000 })
+    }, [])
+
+    const changeBackGround = () =>{
+      if(window.scrollY >= 170){
+        setNavBar(true)
+      }else{
+        setNavBar(false)
+      }
+      console.log(navBar)
+    }
+
+    window.addEventListener('scroll', changeBackGround)
+
+    const navClass = () =>{
+      let classes = "navbar fixed-top navbar-expand-lg navbar-light px-4 "
+      if(navBar){
+        classes += "bg-white py-4"
+      }else if(!navBar){
+        classes += "bg-none py-3"
+      }
+      return classes
+    }
+
     return (
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light p-4 bg-transparent">
+      <nav style={{transition: ".3s"}} className={navClass()}>
         <div className="container">
           <a className="navbar-brand" href="#">Logo</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
