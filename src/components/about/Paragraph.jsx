4135eb6789d50ff96319e81e4ae8016f499cd134
peenest <lanchitavincent@gmail.com>
2020-09-29 18:21:58 +0800

    editing skills and paragraphs

--- src/components/about/Paragraph.jsx
+++ src/components/about/Paragraph.jsx
@@ -12,11 +12,11 @@ const Paragraph = () =>{
             <p className="my-3">
                 Hi! I am vincent lanchita I have a 1 year experience
                 in frontend development and capable in HTML5, CSS3,
-                Bootstrap, JavaScript, jQuery, Reactjs, Nextjs. In addition i 
-                can do also software version control using Git/Github
-                and server side in web, android app, desktop
-                app development such as PHP, MYSQL, SQL Server, C#,
-                C++ and Java.
+                Bootstrap, JavaScript, jQuery, Reactjs. In addition i 
+                have also basic knowledge in software version control using Git/Github
+                and server side in web app, android app, desktop
+                app development such as PHP, MYSQL, SQL Server, Firebase, REST Api
+                C#, C++ and Java.
             </p>
             <a className="btn btn-primary my-3" href="https://dropmb.com/files/13b5f4374bfe138171a30ca9afb87126.docx">Download CV</a>
         </div>
--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ const Data = {
     {
         img: 'html-icon.png',
         service: 'Web Development',
-        description: 'I can build a blog, e-commerce, directory and contact and magazine sites for your product.',
+        description: 'Able to make a responsive layout and user friendly interface.',
         link: "https://en.wikipedia.org/wiki/Web_development"
     },
     {
@@ -17,7 +17,7 @@ const Data = {
     {
         img: 's3.png',
         service: 'Desktop App',
-        description: 'I can build a desktop app just like point of sale, banking and etc. in a modern user interface.',
+        description: 'I can build a desktop app using C# .net technologies with a modern user interface.',
         link: "https://www.google.com/search?sxsrf=ALeKk01tV0RNLK9DoFW8uacv0Qs8OZP7_w%3A1598620444686&ei=HANJX-jKKYyGoATmmIKoDg&q=desktop+application+development+meaning&oq=desktop+application+development+mea&gs_lcp=CgZwc3ktYWIQAxgAMgUIIRCgATIFCCEQoAE6AggAOgQIABBDOgYIABAWEB5QogVYggpg8hZoAHAAeAGAAfgHiAG6FpIBDTAuMS4xLjUtMS4xLjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab"
     }
 
